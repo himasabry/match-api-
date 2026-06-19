be0167ddbd99619c785d217bcc187784605867cd
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-19 22:36:17 +0200

    Delete api directory

--- api/matches.js
+++
@@ -1,9 +0,0 @@
-import { matches } from "../data/matches.js";
-
-export default function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    count: matches.length,
-    matches
-  });
-}
